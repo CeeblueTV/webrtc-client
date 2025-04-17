90775ed607a2b27669efd5eccecf351e66d25539
MathieuPoux <mathieu.poux@ceeblue.net>
2025-04-17 09:04:48 +0200

    build!: update builder to generate correct binaries and support multiple targets and formats
    BREAKING CHANGE: Use webrtc-client.bundle.js for browser inclusion now

--- rollup.config.js
+++ rollup.config.js
@@ -7,90 +7,86 @@
 // For an extensive guide to getting started with the rollup.js JavaScript bundler, visit:
 // https://blog.openreplay.com/the-ultimate-guide-to-getting-started-with-the-rollup-js-javascript-bundler
 
-import replace from '@rollup/plugin-replace';
+import replacer from '@rollup/plugin-replace';
 import eslint from '@rollup/plugin-eslint';
 import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
 import { dts } from 'rollup-plugin-dts';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import autoExternal from 'rollup-plugin-auto-external';
 
 const input = 'index.ts';
 const output = 'dist/webrtc-client';
+const name = 'CeeblueWebRTCClient';
 
 export default args => {
-    let target;
-    let format = args.format;
-    // Determine the target and format based on provided arguments
-    if (format) {
-        if (format.toLowerCase().startsWith('es')) {
-            target = format;
-            format = 'es';
-        } else {
-            target = 'es5';
-        }
-    } else {
-        format = 'es';
-        target = 'es6';
-    }
     // Determine the package version by using the 'version' environment variable (for CI/CD processes) or fallback to the version specified in the 'package.json' file.
-    let version = process.env.version ?? process.env.npm_package_version;
+    const version = process.env.version ?? process.env.npm_package_version;
     // Validate the version format
-    if (typeof version === 'string') {
-        // https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
-        const versionRegex =
-            /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/;
-        if (!versionRegex.test(version)) {
-            throw new Error(
-                'The provided version string does not comply with the Semantic Versioning (SemVer) format required. Please refer to https://semver.org/ for more details on the SemVer specification.'
-            );
-        }
-        console.info('Building version: ' + version);
-    } else {
+    if (typeof version !== 'string') {
         throw new Error('Version is undefined or not a string.');
+    }
+    // https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+    const versionRegex =
+        /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/;
+    if (!versionRegex.test(version)) {
+        throw new Error(
+            'The provided version string does not comply with the Semantic Versioning (SemVer) format required. Please refer to https://semver.org/ for more details on the SemVer specification.'
+        );
+    }
+    if (!args.target) {
+        // ES6 by default
+        args.target = 'es6';
+    }
+    console.info(`Building${args.format ? ' ' + args.format : ''} in ${args.target} version ${version}`);
+
+    const ts = typescript({ target: args.target });
+    delete args.target; // remove the target argument because it not valid for rollup
+    const replace = replacer({
+        __lib__version__: "'" + version + "'",
+        preventAssignment: true
+    });
+
+    function createOutput(input, outputName, ...plugins) {
+        return {
+            input,
+            output: [
+                {
+                    // normal
+                    name,
+                    format: args.format, // iife, es, cjs, umd, amd, system
+                    compact: true,
+                    sourcemap: true,
+                    file: outputName + '.js'
+                },
+                {
+                    // minified
+                    name,
+                    format: args.format, // iife, es, cjs, umd, amd, system
+                    compact: true,
+                    sourcemap: true,
+                    plugins: [terser()],
+                    file: outputName + '.min.js'
+                }
+            ],
+            plugins
+        };
     }
 
     return [
+        // Generate type definitions
         {
-            // Transpile and bundle the code
-            input,
-            output: {
-                name: process.env.npm_package_name,
-                format, // iife, es, cjs, umd, amd, system
-                compact: true,
-                sourcemap: true,
-                file: output + '.js'
-            },
-            plugins: [
-                replace({
-                    __lib__version__: "'" + version + "'",
-                    preventAssignment: true
-                }),
-                eslint(),
-                typescript({ target }),
-                nodeResolve(),
-                commonjs()
-            ]
-        },
-        {
-            // Minify the bundled code
-            input: output + '.js',
-            output: {
-                compact: true,
-                sourcemap: true,
-                file: output + '.min.js'
-            },
-            plugins: [terser()],
-            context: 'window' // Useful for ES5 builds, ensures 'this' refers to 'window' in a browser context
-        },
-        {
-            // Generate type definitions
             input,
             output: {
                 compact: true,
                 file: output + '.d.ts'
             },
             plugins: [dts()]
-        }
+        },
+        // NPM binaries
+        createOutput(input, output, replace, eslint(), ts, commonjs(), autoExternal()),
+        // Browser binaries
+        createOutput(input, output + '.bundle', replace, eslint(), ts, commonjs(), nodeResolve())
     ];
 };
